42755f69f000856c9dce901849ca3d36fdfabccb
ta6 <ta6asko@gmail.com>
2014-10-16 16:19:20 +0300

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+  
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
