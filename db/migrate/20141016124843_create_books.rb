36ef77d49d5ab8a267ca0bf219a1ddc34ac12c83
ta6 <ta6asko@gmail.com>
2014-10-16 18:21:04 +0300

    Add REST contr

--- db/migrate/20141016124843_create_books.rb
+++ db/migrate/20141016124843_create_books.rb
@@ -1,8 +1,20 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-
+      t.string 	:title
+      t.text		:short_description
+			t.text 		:full_description
+			t.float 	:price
+ 
       t.timestamps
+    end
+
+    create_table :authors do |t|
+    	
+    end
+
+    create_table :categories do |t|
+    	
     end
   end
 end
