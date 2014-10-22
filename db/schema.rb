f58a44734bbfcd3eae8be7ae4942310b3fe82038
ta6 <ta6asko@gmail.com>
2014-10-22 18:18:32 +0300

    add_comment&rating

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021175353) do
+ActiveRecord::Schema.define(version: 20141022135717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,11 +46,23 @@ ActiveRecord::Schema.define(version: 20141021175353) do
     t.datetime "updated_at"
   end
 
+  create_table "comments", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "orders", force: true do |t|
     t.string   "in_progress"
     t.string   "in_queue"
     t.string   "in_delivery"
     t.string   "delivered"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ratings", force: true do |t|
+    t.integer  "rating"
+    t.integer  "book_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
